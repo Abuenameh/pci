1527d62c53301146e22425570503dee672e2bdb1
Abuenameh <abuenameh@gmail.com>
2024-04-25 03:00:04 -0400

    Change event handlers

--- mathlive-pci/src/interaction.tsx
+++ mathlive-pci/src/interaction.tsx
@@ -17,27 +17,21 @@ type PropTypes = typeof configProps;
 const Interaction = ({ config, dom, store }: { config: PropTypes; dom: Document | ShadowRoot, store: IStore<StateModel> }) => {
   const mf = useRef<MathfieldElement>();
   const state = useStore(store);
-  const [eventHandled, setEventHandled] = useState<boolean>(false);
 
   dom.addEventListener("changed", (e: CustomEvent) => {
     mf.current.setValue(e.detail);
   })
   
-  const handleInput = (e: Event) => {
-    if (!eventHandled) {
+  const handleChange = (e: Event) => {
       const input = e.target as HTMLInputElement;
       store.dispatch<{ input: string }>({ type: "SET_INPUT", payload: { input: input.value } });
-      setEventHandled(true);
-    }
   };
 
   return <math-field ref={mf}
                      math-virtual-keyboard-policy="auto"
                      onfocusin={() => { // @ts-ignore
                        window.mathVirtualKeyboard.layouts = config.layouts}}
-                     onInput={handleInput}
-                     onPaste={() => setEventHandled(false)}
-                     onKeyDown={() => setEventHandled(false)}
+                     onChange={handleChange}
                      style={{width: config.width, height: config.height, display: config.display}}>
     {state.input}
   </math-field>;
